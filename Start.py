22cee719b6eafa774ffe89be15f371312e5a0e6b
DDC <trump211dev@gmail.com>
2013-03-12 19:56:03 -0400

    Added Options for command Line operation

--- Start.py
+++ Start.py
@@ -1,4 +1,6 @@
 import sys,inspect,os,traceback,datetime
+from argparse import ArgumentParser
+
 scriptDir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 sys.path.append(scriptDir+"\\Board")
 sys.path.append(scriptDir+"\\Library")
@@ -30,11 +32,11 @@ def startGui():
 	label = Label(frame, font=20, bg="BLUE", text="What Would You Like to Do:")
 	label.pack(fill=X, expand=1)
 	
-	butBrd = Button(frame, text="Convert Board",command=convertBoard)
+	butBrd = Button(frame, text="Convert Board",command=convertBoardGUI)
 	butBrd.pack(fill=X,expand=1)
-	butLib = Button(frame, text="Convert Library",command=convertLib)
+	butLib = Button(frame, text="Convert Library",command=convertLibGUI)
 	butLib.pack(fill=X,expand=1)
-	butSch = Button(frame, text="Convert Schematic",command=convertSch)
+	butSch = Button(frame, text="Convert Schematic",command=convertSchGUI)
 	butSch.pack(fill=X,expand=1)
 	
 	label = Label(frame,bg="BLUE", text="www.github.com/Trump211")
@@ -47,11 +49,22 @@ def getRootNode(fileName):
 	node = node.getroot()
 	return node
 
-def convertBoard():
+def convertBoardGUI():
+	fileName=askopenfilename(title = "Board Input",
+					filetypes=[('Eagle V6 Board', '.brd'), ('all files', '.*')],
+					defaultextension='.brd')
 	
-	fileName=askopenfilename(title = "Board Input", filetypes=[('Eagle V6 Board', '.brd'), ('all files', '.*')], defaultextension='.brd') 
-	outFileName=asksaveasfilename(title="Board Output", filetypes=[('KiCad Board', '.brd'), ('all files', '.*')], defaultextension='.brd')
+	outFileName=asksaveasfilename(title="Board Output",
+					filetypes=[('KiCad Board', '.brd'), ('all files', '.*')],
+					defaultextension='.brd')
 	
+	val = convertBoard(fileName,outFileName)
+	if val[0]:
+		showinfo("Conversion Complete",val[1])
+	else:
+		showerror("Error",val[1])
+
+def convertBoard(fileName,outFileName):
 	logFile.write("*******************************************\n")
 	logFile.write("Converting: "+fileName+"\n")
 	logFile.write("Outputing: "+outFileName+"\n\n")
@@ -65,22 +78,39 @@ def convertBoard():
 		brd.write(outFile)
 		outFile.close()
 	
-	except Exception as e:
-		showerror("Error",str(e)+"\nSee Log.txt for more info")		
+	except BaseException as e:	
 		logFile.write("Conversion Failed\n\n")
 		logFile.write(traceback.format_exc())
 		logFile.write("*******************************************\n\n\n")
-		return
+		return False, "Error Converting Board \n"+str(e)+\
+					  "\nSee Log.txt for more info"
+
 	
 	logFile.write("Conversion Successfull\n\n")
 	logFile.write("*******************************************\n\n\n")	
-	showinfo("Board Complete","The Board Has Finished Converting")
+	return True,"The Board Has Finished Converting"
+
+def convertLibGUI():
+	fileName=askopenfilename(title = "Input Library",
+				filetypes=[('Eagle V6 Library', '.lbr'),('all files', '.*')],
+				defaultextension='.lbr')
 	
-def convertLib():
-	fileName=askopenfilename(title = "Input Library",filetypes=[('Eagle V6 Library', '.lbr'), ('all files', '.*')], defaultextension='.lbr') 
-	modFileName=asksaveasfilename(title="Module Output Filename", filetypes=[('KiCad Module', '.mod'), ('all files', '.*')], defaultextension='.mod')
-	symFileName=asksaveasfilename(title="Symbol Output Filename", filetypes=[('KiCad Symbol', '.lib'), ('all files', '.*')], defaultextension='.lib')
+	modFileName=asksaveasfilename(title="Module Output Filename", 
+					filetypes=[('KiCad Module', '.mod'), ('all files', '.*')], 
+					defaultextension='.mod')
 	
+	symFileName=asksaveasfilename(title="Symbol Output Filename", 
+					filetypes=[('KiCad Symbol', '.lib'), ('all files', '.*')], 
+					defaultextension='.lib')
+	
+	val = convertLib(fileName,symFileName,modFileName)
+	if val[0]:
+		showinfo("Conversion Complete",val[1])
+	else:
+		showerror("Error",val[1])
+		
+def convertLib(fileName,symFileName,modFileName):
+
 	logFile.write("*******************************************\n")
 	logFile.write("Converting Lib: "+fileName+"\n")
 	logFile.write("Module Output: "+modFileName+"\n")
@@ -90,7 +120,7 @@ def convertLib():
 	name=name.split("\\")[-1]
 	name=name.split(".")[0]
 	
-	logFile.write("Lib name: "+name+"\n")
+	logFile.write("Lib Name: "+name+"\n")
 	
 	try:
 		node = getRootNode(fileName)
@@ -98,7 +128,8 @@ def convertLib():
 		lib=Library(node,name)
 		
 		open(modFileName,'w').close()
-		open(symFileName,'w').close()		
+		open(symFileName,'w').close()
+				
 		modFile=open(modFileName,"a")
 		symFile=open(symFileName,"a")
 					
@@ -106,31 +137,100 @@ def convertLib():
 		
 		modFile.close()
 		symFile.close()
-	except Exception as e:
-		showerror("Error",str(e)+"\nSee Log.txt for more info")		
+	except BaseException as e:	
 		logFile.write("Conversion Failed\n\n")
 		logFile.write(traceback.format_exc())
 		logFile.write("*******************************************\n\n\n")
-		return
+		
+		return False, "Error Converting Library: '"+name+"'\n"+str(e)+"\nSee Log.txt for more info"	
+
 	
 	logFile.write("Conversion Successfull\n")
 	logFile.write("*******************************************\n\n\n")		
-	showinfo("Library Complete","The Modules and Symbols Have Finished Converting \n Check Console for Errors")
-		
-def convertSch():
+	
+	return True, "Conversion of Library '"+name+"' Complete"
+
+def convertSchGUI():
+	val=convertSch("N/A","N/A")
+	if val[0]:
+		showinfo("Conversion Complete",val[1])
+	else:
+		showerror("Error",val[1])
+			
+def convertSch(schFile,outFile):
 	logFile.write("*******************************************\n")
 	logFile.write("Converting Schem: \n")
 	logFile.write("Conversion Failed: \n\n")
 	logFile.write("Schematic Conversion not yet Supported\n\n")
 	logFile.write("*******************************************\n\n\n")	
 	
-	showerror("Error","Converting Schematics is not yet supported")
+	return False, "Converting Schematics is not yet supported"
 
-if __name__ == "__main__":
+def startCmdLine():
+		# Setup argument parser
+		parser = ArgumentParser()
+		parser.add_argument("-l","-L","--Library", 
+						dest="Library", 
+						nargs=3,
+						metavar=("inFile","symFile","modFile"), 
+						help="Convert an Eagle Library",
+						action="append",
+						type=str)
+		
+		parser.add_argument("-b","-B","--Board",
+						dest="Board",
+						nargs=2,
+						metavar=("inFile","brdFile"),
+						help="Convert an Eagle Board",
+						action="append",
+						type=str)
+		
+		parser.add_argument("-s","-S","--Schematic",
+						dest="Schem",
+						nargs=2,
+						metavar=("inFile","schFile"),
+						help="Convert an Eagle Schematic",
+						action="append",
+						type=str)
+		
+		# Process arguments
+		args = parser.parse_args()
+		
+		if args.Schem != None:
+			for sch in args.Schem:
+				val = convertSch(sch[0],sch[1])
+				if val[0]:
+					sys.stdout.write(val[1]+"\n")
+				else:
+					sys.stderr.write(val[1]+"\n")
+					
+		if args.Board != None:
+			for brd in args.Board:
+				val = convertBoard(brd[0],brd[1])
+				if val[0]:
+					sys.stdout.write(val[1]+"\n")
+				else:
+					sys.stderr.write(val[1]+"\n")
+		
+		if args.Library != None:
+			for lib in args.Library:
+				val = convertLib(lib[0],lib[1],lib[2])
+				if val[0]:
+					sys.stdout.write(val[1]+"\n")
+				else:
+					sys.stderr.write(val[1]+"\n")
+								
+def main():
 	now=datetime.datetime.now()
 	logFile.write("###############################################################################\n")
 	logFile.write("#Session: "+now.strftime("%Y-%m-%d %H:%M:%S")+"\n")
 	logFile.write("###############################################################################\n\n\n")
 	
-	startGui()
+	if len(sys.argv)>1:
+		startCmdLine()
+	else:
+		startGui()
+		
+if __name__ == "__main__":
+	main()
 			
